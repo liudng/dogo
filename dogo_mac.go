d85a3edf9d9414440e24c298fc61323255aeeb54
vanishs <vanishs@hotmail.com>
2015-06-07 18:07:56 +0800

    add support mac

---
+++ dogo_mac.go
@@ -0,0 +1,51 @@
+// Copyright 2014 The dogo Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"os"
+	"path/filepath"
+	"time"
+)
+
+func (d *Dogo) Monitor() {
+	for {
+		d.Compare()
+
+		if d.isModified == true {
+			d.BuildAndRun()
+		}
+
+		time.Sleep(time.Duration(1 * time.Second))
+	}
+}
+
+//compare source file's modify time
+func (d *Dogo) Compare() {
+	changed := false
+
+	for p, t := range d.Files {
+		info, err := os.Stat(p)
+		if err != nil {
+			d.FmtPrintf("%s\n", err)
+			continue
+		}
+
+		//new modtime
+		nt := info.ModTime()
+
+		if nt.Sub(t) > 0 {
+			d.Files[p] = nt
+			changed = true
+			d.FmtPrintf("[dogo] Changed files: %s\n", filepath.Base(p))
+		}
+	}
+
+	if changed == true {
+		d.isModified = true
+	} else {
+		d.isModified = false
+	}
+}
